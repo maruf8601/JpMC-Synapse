cd26a67ecbe214f9d6ee22238556e604d3c132b8
Abdullah Al Maruf <marufjb@gmail.com>
2026-09-17 21:09:53 +0000

    Add Android Capacitor build

---
+++ android/app/src/main/java/bd/gov/jpmc/synapse/MainActivity.java
@@ -0,0 +1,5 @@
+package bd.gov.jpmc.synapse;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'bd.gov.jpmc.synapse',
+  appName: 'JpMC Synapse',
+  webDir: 'dist'
+};
+
+export default config;
